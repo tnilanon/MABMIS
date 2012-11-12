92da5aa0af0690fac2f8447a3c167aa3800ebc4d
Tanachat Nilanon <gor@cs.unc.edu>
2012-11-12 10:30:38 -0500

    - Done ROI merger in MATLAB

--- src/MATLAB/MergeRegionsOfInterestLONILPBA40.m
+++ src/MATLAB/MergeRegionsOfInterestLONILPBA40.m
@@ -3,6 +3,35 @@
 % IDEA Lab
 % Nov 9, 2012
 
-function MergeRegionsOfInterestLONILPBA40 ( filename )
+function MergeRegionsOfInterestLONILPBA40 ( filenameWithoutExtension )
+    header = readAnalyzeHeader([filenameWithoutExtension, '.hdr']);
+    
+    fIn = fopen([filenameWithoutExtension, '.img'], 'r');
+    if header.image_dimension.datatype == 2
+        I = fread(fIn, '*uchar');
+    else
+        error('Data format not understood. Please check MATLAB code.');
+    end
+    fclose(fIn);
+    
+    I(I==21|I==26|I==31|I==34|I==46|I==49|I==61|I==68) = 1;
+    I(I==22|I==25|I==32|I==33|I==45|I==50|I==62|I==67) = 2;
+    I(I==27|I==28|I==29|I==30|I==43|I==44|I==181) = 3;
+    I(I==23|I==24|I==41|I==42|I==47|I==48|I==182) = 4;
+    I(I==65|I==66|I==81|I==82|I==87|I==88|I==163|I==164) = 5;
+    I(I==83|I==84|I==91|I==92|I==161|I==162) = 6;
+    I(I==63|I==85|I==90|I==102|I==121|I==166) = 7;
+    I(I==64|I==86|I==89|I==101|I==122|I==165) = 8;
+    
+    assert(copyfile([filenameWithoutExtension, '.hdr'], ...
+        [filenameWithoutExtension, '_group.hdr']));
+    
+    fOut = fopen([filenameWithoutExtension, '_group.img'], 'w');
+    if header.image_dimension.datatype == 2
+        fwrite(fOut, I, '*uchar');
+    else
+        error('Data format not understood. Please check MATLAB code.');
+    end
+    fclose(fOut);
 end
 
---
+++ src/MATLAB/readAnalyzeHeader.m
@@ -0,0 +1,61 @@
+% Tanachat Nilanon
+% gor@cs.unc.edu
+% IDEA Lab
+% Nov 12, 2012
+
+function header = readAnalyzeHeader ( filename )
+    fp = fopen(filename, 'r');
+    
+    header.header_key.sizeof_hdr =      fread(fp, 1,    '*int32');
+    header.header_key.data_type =       fread(fp, 10,   '*char*1');
+    header.header_key.db_name =         fread(fp, 18,   '*char*1');
+    header.header_key.extents =         fread(fp, 1,    '*int32');
+    header.header_key.session_error =   fread(fp, 1,    '*int16');
+    header.header_key.regular =         fread(fp, 1,    '*char*1');
+    header.header_key.hkey_un0 =        fread(fp, 1,    '*uchar');
+    
+    header.image_dimension.dim =        fread(fp, 8,    '*int16');
+    header.image_dimension.unused8 =    fread(fp, 1,    '*int16');
+    header.image_dimension.unused9 =    fread(fp, 1,    '*int16');
+    header.image_dimension.unused10 =   fread(fp, 1,    '*int16');
+    header.image_dimension.unused11 =   fread(fp, 1,    '*int16');
+    header.image_dimension.unused12 =   fread(fp, 1,    '*int16');
+    header.image_dimension.unused13 =   fread(fp, 1,    '*int16');
+    header.image_dimension.unused14 =   fread(fp, 1,    '*int16');
+    header.image_dimension.datatype =   fread(fp, 1,    '*int16');
+    header.image_dimension.bitpix =     fread(fp, 1,    '*int16');
+    header.image_dimension.dim_un0 =    fread(fp, 1,    '*int16');
+    header.image_dimension.pixdim =     fread(fp, 8,    '*float32');
+    header.image_dimension.vox_offset = fread(fp, 1,    '*float32');
+    header.image_dimension.funused1 =   fread(fp, 1,    '*float32');
+    header.image_dimension.funused2 =   fread(fp, 1,    '*float32');
+    header.image_dimension.funused3 =   fread(fp, 1,    '*float32');
+    header.image_dimension.cal_max =    fread(fp, 1,    '*float32');
+    header.image_dimension.cal_min =    fread(fp, 1,    '*float32');
+    header.image_dimension.compressed = fread(fp, 1,    '*float32');
+    header.image_dimension.verified =   fread(fp, 1,    '*float32');
+    header.image_dimension.glmax =      fread(fp, 1,    '*int32');
+    header.image_dimension.glmin =      fread(fp, 1,    '*int32');
+    
+    header.data_history.descrip =       fread(fp, 80,   '*char*1');
+    header.data_history.aux_file =      fread(fp, 24,   '*char*1');
+    header.data_history.orient =        fread(fp, 1,    '*uchar');
+    header.data_history.originator =    fread(fp, 10,   '*char*1');
+    header.data_history.generated =     fread(fp, 10,   '*char*1');
+    header.data_history.scannum =       fread(fp, 10,   '*char*1');
+    header.data_history.patient_id =    fread(fp, 10,   '*char*1');
+    header.data_history.exp_date =      fread(fp, 10,   '*char*1');
+    header.data_history.exp_time =      fread(fp, 10,   '*char*1');
+    header.data_history.hist_un0 =      fread(fp, 3,    '*uchar');
+    header.data_history.views =         fread(fp, 1,    '*int32');
+    header.data_history.vols_added =    fread(fp, 1,    '*int32');
+    header.data_history.start_field =   fread(fp, 1,    '*int32');
+    header.data_history.field_skip =    fread(fp, 1,    '*int32');
+    header.data_history.omax =          fread(fp, 1,    '*int32');
+    header.data_history.omin =          fread(fp, 1,    '*int32');
+    header.data_history.smax =          fread(fp, 1,    '*int32');
+    header.data_history.smin =          fread(fp, 1,    '*int32');
+    
+    fclose(fp);
+end
+
